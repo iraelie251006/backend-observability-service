3a350ebb678c1aff5234167f068faf096153b095
Elie <niyubwayoiraelie5777@gmail.com>
2026-05-08 23:42:20 +0200

    implemented MdcKeys immutable class for constant reusable variables in detailed logging.

---
+++ src/main/java/tech/iraelie/practice/logger/MdcKeys.java
@@ -0,0 +1,10 @@
+package tech.iraelie.practice.logger;
+
+public final class MdcKeys {
+    public static final String CORRELATION_ID = "correlationId";
+    public static final String TRACE_ID = "traceId";
+    public static final String SPAN_ID = "spanId";
+    public static final String USER_ID = "userId";
+    public static final String SERVICE = "service";
+    private MdcKeys() {}
+}
