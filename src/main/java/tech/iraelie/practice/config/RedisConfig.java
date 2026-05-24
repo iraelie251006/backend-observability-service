013a7f656b372e547a9e006e073f446d58c9828f
Elie <niyubwayoiraelie5777@gmail.com>
2026-05-24 23:46:33 +0200

    Implemented Redis Configuration by specifying the way data will be serialized mostly in human-readable format avoiding complex debugging issues.

--- src/main/java/tech/iraelie/practice/config/RedisConfig.java
+++ src/main/java/tech/iraelie/practice/config/RedisConfig.java
@@ -1,9 +1,51 @@
 package tech.iraelie.practice.config;
 
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJacksonJsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import tools.jackson.databind.cfg.DateTimeFeature;
+import tools.jackson.datatype.jsr310.JavaTimeModule;
+import tools.jackson.databind.json.JsonMapper;
+
+import java.time.Duration;
 
 @Configuration
 @EnableCaching
 public class RedisConfig {
+    @Bean
+    public CacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+        GenericJacksonJsonRedisSerializer jsonSerializer = GenericJacksonJsonRedisSerializer.builder(
+                        () -> JsonMapper.builder()
+                                .addModule(new JavaTimeModule())
+                                .configure(DateTimeFeature.WRITE_DATES_AS_TIMESTAMPS, false)
+                )
+                .enableSpringCacheNullValueSupport()
+                .build();
+
+
+        RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(10))
+                .serializeKeysWith(
+                        RedisSerializationContext
+                                .SerializationPair
+                                .fromSerializer(new StringRedisSerializer())
+                )
+                .serializeValuesWith(
+                        RedisSerializationContext
+                                .SerializationPair
+                                .fromSerializer(jsonSerializer)
+                )
+                .disableCachingNullValues();
+
+        return RedisCacheManager.builder(connectionFactory)
+                .cacheDefaults(config)
+                .build();
+    }
 }
