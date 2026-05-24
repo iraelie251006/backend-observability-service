8a87b97b8ff78cbf47903119c9004e3f05600742
Elie <niyubwayoiraelie5777@gmail.com>
2026-05-24 23:40:17 +0200

    Added enabled caching annotation on main application entry point with @EnableCaching.

--- src/main/java/tech/iraelie/practice/PracticeApplication.java
+++ src/main/java/tech/iraelie/practice/PracticeApplication.java
@@ -2,8 +2,10 @@ package tech.iraelie.practice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class PracticeApplication {
 
 	public static void main(String[] args) {
