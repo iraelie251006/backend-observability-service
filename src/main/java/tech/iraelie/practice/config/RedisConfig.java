b6201b6aa442bdbe56442c1ad752b59d8a023883
Elie <niyubwayoiraelie5777@gmail.com>
2026-05-25 23:51:29 +0200

    created placeholder redisConnectionFactory that return LettuceConnectionFactory for connection pooling.

--- src/main/java/tech/iraelie/practice/config/RedisConfig.java
+++ src/main/java/tech/iraelie/practice/config/RedisConfig.java
@@ -6,7 +6,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.serializer.GenericJacksonJsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -20,7 +21,7 @@ import java.time.Duration;
 @EnableCaching
 public class RedisConfig {
     @Bean
-    public CacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+    public CacheManager cacheManager(LettuceConnectionFactory connectionFactory) {
         GenericJacksonJsonRedisSerializer jsonSerializer = GenericJacksonJsonRedisSerializer.builder(
                         () -> JsonMapper.builder()
                                 .addModule(new JavaTimeModule())
@@ -47,5 +48,9 @@ public class RedisConfig {
         return RedisCacheManager.builder(connectionFactory)
                 .cacheDefaults(config)
                 .build();
+    }
+
+    @Bean
+    public LettuceConnectionFactory redisConnectionFactory () {
     }
 }
